c32d82a5c328876735c75c5a166dc543a693b18f
agdeva8 <agarwaldevanshu8@gmail.com>
2018-08-07 16:37:34 +0530

    descending order ques2 completed

---
+++ assignment_1/descending_basic.asm
@@ -0,0 +1,36 @@
+	.data
+a:
+	70
+	80
+	40
+	20
+	10
+	30
+	50
+	60
+n:
+	8
+	.text
+main:
+	addi %x0, 1, %x3
+	load %x0, $n, %x8
+loopmain:
+	add %x0, %x0, %x4
+	addi %x0, 1, %x5
+loopsub:
+	load %x4, $a, %x6
+	load %x5, $a, %x7
+	bgt %x6, %x7, noswap
+	store %x7, $a, %x4
+	store %x6, $a, %x5
+noswap:
+	addi %x4, 1, %x4
+	addi %x5, 1, %x5
+	beq %x5, %x8, checksorted
+	jmp loopsub
+checksorted:
+	addi %x3, 1, %x3
+	beq %x3, %x8, endl
+	jmp loopmain
+endl:
+	end
