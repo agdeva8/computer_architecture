1cbe5b27ed2347a4531d77c0a7232c507d6bde99
agdeva8 <agarwaldevanshu8@gmail.com>
2018-12-25 19:50:37 +0530

    removed

--- assignment_1/supporting_files/program_templates/even-odd_template.asm
+++
@@ -1,8 +0,0 @@
-	.data
-a:
-	10
-	.text
-// your code here to check if the number 'a' is even or odd. If yes, write 1 to x10. Else write -1.
-// you may change the value of 'a'
-// remove these comments!
-
